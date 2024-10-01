7129cbabd199f0ccf7a5deeb3e6e59c775a89e3a
Leonid <zemlyanykh@bk.ru>
2024-10-02 00:49:31 +0500

    Исправление ошибки

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,15 +1,15 @@
-const visibleBlock = document.querySelector('.reveal');
+const visibleBlock = document.querySelector('.reveal')
 
 document.addEventListener('scroll', function() {
-    const { top, bottom } = visibleBlock.getBoundingClientRect();
+    const {top, bottom} = visibleBlock.getBoundingClientRect();
 
     if (bottom < 0) {
-        return false;
-    } 
-
-    if (top > innerHeight) {
-        return false;
+        return false
     }
 
-    return visibleBlock.classList.add('reveal_active')
+    if (top > innerHeight) {
+        return false
+    }
+
+    visibleBlock.classList.add('reveal_active')
 })
